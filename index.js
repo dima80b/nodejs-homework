8dfd4894ba974f1eb5318eb356c39cdae6764c77
dima_burch <dima80b@gmail.com>
2020-08-24 17:23:02 +0300

    Change index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 let dataTxT = fs.readFileSync("hello.txt");
+dataTxT = dataTxT.toString();
+dataTxT = dataTxT.split("\n");
 
-console.log(dataTxT.toString());
+console.log(dataTxT);
 
 
